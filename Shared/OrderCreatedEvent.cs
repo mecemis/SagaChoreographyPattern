19dd0f4377bf73dd332ba75adf2aca03524b25d4
mecemis <ecemis.murat@gmail.com>
2021-08-19 18:21:23 +0300

    Add project files.

---
+++ Shared/OrderCreatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Shared
+{
+    public class OrderCreatedEvent
+    {
+        public int OrderId { get; set; }
+        public string BuyerId { get; set; }
+
+        public PaymentMessage Payment { get; set; }
+        public List<OrderItemMessage> orderItems { get; set; } = new List<OrderItemMessage>();
+    }
+}
